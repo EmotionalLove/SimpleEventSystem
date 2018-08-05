ed9088ae51e71eeb68a081d04292265f267dfd5d
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-05 16:30:29 -0700

    Prevent registration of the same event twice.

--- src/com/sasha/eventsys/SimpleEventManager.java
+++ src/com/sasha/eventsys/SimpleEventManager.java
@@ -4,6 +4,8 @@ import com.sasha.eventsys.exception.ListenerRegistrationException;
 import com.sun.istack.internal.NotNull;
 
 import javax.management.ListenerNotFoundException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -30,21 +32,33 @@ public class SimpleEventManager {
             if ((!method.getParameterTypes()[0].getSuperclass().getName().matches("com\\.sasha\\.eventsys\\.SimpleEvent|com\\.sasha\\.eventsys\\.SimpleCancellableEvent"))){
                 throw new ListenerRegistrationException("Function " + method.getName() + " has invalid type of parameter (" + method.getParameterTypes()[0].getSuperclass().getName() +")");
             }
+            if (registeredMethods.contains(method)){
+                return;
+            }
             registeredMethods.add(method);
         }
     }
     public void invokeEvent(SimpleEvent e){
-        registeredMethods.forEach(method -> {
-            if (method.getParameterTypes()[0] == e.getClass()){
+        for (Method method : registeredMethods) {
+            if (method.getParameterTypes()[0] == e.getClass()) {
                 try {
                     method.setAccessible(true);
-                    method.invoke(method.getDeclaringClass().newInstance(), e);
-                    method.setAccessible(false);
-                }catch (Exception ex){
+                    Class clasz = method.getDeclaringClass();
+                    for (Field field : clasz.getFields()) {
+                        field.setAccessible(true);
+                    }
+                    for (Method meth : clasz.getMethods()) {
+                        meth.setAccessible(true);
+                    }
+                    for (Constructor<?> constructor : clasz.getConstructors()) {
+                        constructor.setAccessible(true);
+                    }
+                    method.invoke(clasz.newInstance(), e);
+                } catch (Exception ex) {
                     System.out.println("FATAL EXCEPTION DURING " + e.getClass().getName() + "'s EXECUTION");
                     ex.printStackTrace();
                 }
             }
-        });
+        }
     }
 }
